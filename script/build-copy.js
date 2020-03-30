40e4f4ab13b5dc2e4c14838fff67ca17c3f1a846
mac <mac@10.0.0.2>
2020-03-30 16:43:53 +0800

    .

--- script/build-copy.js
+++ script/build-copy.js
@@ -14,7 +14,7 @@ rm('*.js', err => {
   if (err) throw (err)
   let folderList = fs.readdirSync(path.resolve(rootPath, 'src'))
   folderList.forEach((item, index) => {
-    copy(`src/${item}/*.js`, rootPath, function (err, files) {
+    copy(`src/${item}/*.js`, `${rootPath}/dist`, function (err, files) {
       if (err) throw err;
       if (index === folderList.length - 1) {
         console.log(chalk.cyan('  Copy complete.\n'))
